6e08a6360e467e379f154c0fb20ee1bcb7fc4cca
CL/BSCSD/33/83 <yasindupawan0617@gmail.com>
2026-03-02 15:20:40 +0530

    database connection done

---
+++ src/main/java/com/ocean/view/resort/oceanviewresortapp/util/DBConnection.java
@@ -0,0 +1,20 @@
+package com.ocean.view.resort.oceanviewresortapp.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/oceanviewresortdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver not found", e);
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
